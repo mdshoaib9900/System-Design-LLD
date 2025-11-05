7ae74bfccb23a569525162f6a0d8bedd83873b6c
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-11-05 22:34:31 +0530

    updated Command pattern

--- Behavioural Design Patterns/MediatorPattern.java
+++ Behavioural Design Patterns/MediatorPattern.java
@@ -1,3 +1,45 @@
+
+import java.util.ArrayList;
+
+interface  Mediator{
+    void registerBidder(Bidder bidder);
+    void placeBid(Bidder bidder,int amount);
+}
+class AuctionHouse implements Mediator{
+    ArrayList<Bidder> bidders=new ArrayList<>();
+    @Override
+    public void registerBidder(Bidder bidder) {
+        bidders.add(bidder);
+    }
+
+    @Override
+    public void placeBid(Bidder bidder, int amount) {
+        System.out.println(bidder.getName()+" placed a bid of amount "+amount);
+        for(Bidder b:bidders){
+            if(b!=bidder){
+                b.recievedBid(bidder,amount);
+            }
+        }
+    }
+
+}
+class Bidder{
+    private String name;
+    private Mediator mediator;
+    public Bidder(Mediator mediator,String name){
+        this.mediator=mediator;
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void placeBid(int amount){
+        mediator.placeBid(this, amount);
+    }
+    public void recievedBid(Bidder bidder,int amount){
+        System.out.println(name + "is Notififed "+bidder.getName() + "placed a bid of "+amount );
+    }
+}
 public class MediatorPattern{
     public static void main(String[] args) {
         
