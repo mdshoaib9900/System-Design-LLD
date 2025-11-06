8b52530a06fca345d96ae12f5b968d40a5568ef3
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-11-06 23:11:28 +0530

    updated Command pattern

--- Behavioural Design Patterns/TemplatePattern.java
+++ Behavioural Design Patterns/TemplatePattern.java
@@ -1,4 +1,20 @@
+abstract class Beverage{
+    final void Beverage(){
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+    void boilWater(){
+        System.err.println("Boiling water..");
+    }
+    void pourInCup(){
+        System.err.println("Pouring in cup..");
 
+    }
+    abstract void brew();
+    abstract void addCondiments();
+}
 public class TemplatePattern{
     public static void main(String[] args) {
         
