d5d1db29776e65700d9e12e9be06aeac14e605de
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-11-06 23:45:39 +0530

    updated Command pattern

---
+++ Behavioural Design Patterns/ChainOfResponsibility.java
@@ -0,0 +1,51 @@
+abstract class Approver{
+    protected Approver nextApprover;
+    public void setNextApprover(Approver nextApprover){
+        this.nextApprover=nextApprover;
+    }
+
+    public abstract void processLeaveRequest(int leaveDays);
+}
+class Supervisor extends Approver{
+
+    @Override
+    public void processLeaveRequest(int leaveDays) {
+        if(leaveDays<=3){
+            System.out.println("Supervisor approved the leave");
+        }else if(nextApprover!=null){
+            nextApprover.processLeaveRequest(leaveDays);
+        }
+    }
+
+}
+class Manager extends Approver{
+
+    @Override
+    public void processLeaveRequest(int leaveDays) {
+        if(leaveDays<=7){
+            System.out.println("Manager approved the leave");
+        }else if(nextApprover!=null){
+            nextApprover.processLeaveRequest(leaveDays);
+        }
+    }
+
+}
+class Director extends Approver{
+
+    @Override
+    public void processLeaveRequest(int leaveDays) {
+        if(leaveDays<=14){
+            System.out.println("Director approved the leave");
+        }else if(nextApprover!=null){
+            nextApprover.processLeaveRequest(leaveDays);
+        }else{
+            System.out.println("leave request denied becuz too many days");
+        }
+    }
+
+}
+public class ChainOfResponsibility{
+    public static void main(String[] args) {
+        
+    }
+}
