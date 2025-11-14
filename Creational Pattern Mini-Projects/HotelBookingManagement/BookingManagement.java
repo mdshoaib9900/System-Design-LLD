ae68127e640f6d222304159ea391e3a51a6b5d41
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-11-14 22:34:07 +0530

    updated management system

--- Creational Pattern Mini-Projects/HotelBookingManagement/BookingManagement.java
+++ Creational Pattern Mini-Projects/HotelBookingManagement/BookingManagement.java
@@ -1,3 +1,18 @@
+abstract class Room{
+    protected String type;
+    protected double price;
+
+    public String getType(){
+        return type;
+    }
+    public double getPrice(){
+        return price;
+    }
+
+    public String toString(){
+        return type+" Room (₹" + price + ")";
+    }
+}
 public class BookingManagement{
     public static void main(String[] args) {
         
