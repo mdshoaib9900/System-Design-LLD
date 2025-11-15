a93297717dd886fc265b86790b33b6c03f702a75
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-11-15 22:05:53 +0530

    updated management system

--- Structral Design Patterns/AdapterPattern.java
+++ Structral Design Patterns/AdapterPattern.java
@@ -2,7 +2,20 @@ interface SmartDevice{
     void turnOn();
     void turnOf();
 }
-
+class Airconditioning{
+    public void connectedViaBluetooth(){
+        System.out.println("AC connected via bluetooth");
+    }
+    public void startCooling(){
+        System.out.println("Ac has started cooling..");
+    }
+    public void stopCooling(){
+        System.out.println("Ac has stopped cooling..");
+    }
+    public void disconnectBluetooth(){
+        System.out.println("Ac has been disconnected from bluetooth");
+    }
+}
 public class AdapterPattern{
     public static void main(String[] args) {
         
