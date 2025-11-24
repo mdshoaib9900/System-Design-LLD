9032c13ed333a7044c363a3528d00470de4ed86d
mdshoaib9900 <mdshoaib6360@gmail.com>
2025-11-24 22:00:45 +0530

    updated

---
+++ Structral Design Patterns/ProxyPattern.java
@@ -0,0 +1,50 @@
+
+import java.util.*;
+
+interface VideoServiceInterface{
+    void playVideo(String userType,String videoName);
+}
+class RealVideoService implements VideoServiceInterface{
+
+    @Override
+    public void playVideo(String userType, String videoName) {
+       System.out.println("Streaming video "+videoName);
+    }
+
+}
+class ProxyVideoService implements  VideoServiceInterface{
+    private  RealVideoService realVideoService;
+    private Map<String,String> cachedVideos=new HashMap<>();
+    private Map<String,Integer> requestCount=new HashMap<>();
+
+    public ProxyVideoService(RealVideoService realVideoService){
+        this.realVideoService=realVideoService;
+    }
+
+
+    @Override
+    public void playVideo(String userType, String videoName) {
+        if(!userType.equals("premium") && videoName.startsWith("premium")){
+            System.out.println("Access denied :premium video requires premium services ");
+            return;
+        }
+        requestCount.put(userType, requestCount.getOrDefault(userType,0)+1);
+        if(requestCount.get(userType)>3){
+            System.out.println("Access denied..");
+            return;
+        }
+        if(cachedVideos.containsKey(videoName)){
+            System.out.println("Streaming cached video "+videoName);
+        }else{
+            realVideoService.playVideo(userType, videoName);
+            cachedVideos.put(videoName, videoName);
+        }
+    }
+
+
+}
+public class ProxyPattern{
+    public static void main(String[] args) {
+        
+    }
+}
